147131fcaa0f737431082189550f5783ad171818
Guilherme Gomes <gui.rock08@gmail.com>
2023-06-05 23:18:19 -0300

    Adding new triangle methods

--- src/Medium/Triang/TriangMensureWithClasses.java
+++ src/Medium/Triang/TriangMensureWithClasses.java
@@ -14,11 +14,13 @@ public class TriangMensureWithClasses {
             x.a[i] = sc.nextDouble();
             x.b[i] = sc.nextDouble();
 
-            x.result[i] = (x.a[i] * x.b[i]) / 2;
+            x.result[i] = x.TriangArea(x.a[i], x.b[i]);
+
             System.out.printf("Área do triângulo %d: %.2f\n", i + 1, x.result[i]);
         }
 
-        int triangleWithLargerArea = (x.result[0] > x.result[1]) ? 1 : 2;
+        int triangleWithLargerArea = x.CompareTriang(x.result[0], x.result[1]);
+
         System.out.printf("A área do triângulo %d é maior\n", triangleWithLargerArea);
     }
 }
--- src/Medium/Triang/Triangle.java
+++ src/Medium/Triang/Triangle.java
@@ -4,4 +4,12 @@ class Triangle {
     public double[] a = new double[2];
     public double[] b = new double[2];
     public double[] result = new double[2];
+
+    public double TriangArea(double num1, double num2) {
+        return (num1 * num2) / 2;
+    }
+
+    public int CompareTriang(double num1, double num2) {
+        return (num1 > num2) ? 1 : 2;
+    }
 }
