59bef9efc1c92ef81a351b814ec6d5ccb52a9261
Guilherme Gomes <gui.rock08@gmail.com>
2023-08-17 15:37:41 -0300

    Adding encapsulation concepts

---
+++ src/medium/terceiro_produto/Program.java
@@ -0,0 +1,42 @@
+package medium.terceiro_produto;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Price: ");   
+        Double price = sc.nextDouble();
+
+        Product product = new Product(name, price);
+
+        product.setName(name+"s");
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        sc.close();
+    }
+}
